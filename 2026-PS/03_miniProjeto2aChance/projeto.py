fd4cc41384b7b3d4a976aeb9bfbfb2001ff353da
20251ctb0100035-ai <20251ctb0100035@estudantes.ifpr.edu.br>
2026-04-07 15:00:02 +0000

    mini-projeto: versão final

--- 2026-PS/03_miniProjeto2aChance/projeto.py
+++ 2026-PS/03_miniProjeto2aChance/projeto.py
@@ -34,49 +34,49 @@ def salvar(resultado):
         print(f"Erro ao salvar o resultado: {e}")
 
 # Verificar ganha
-def verificar_ganha(valor):     
+def verificar_ganha(valor):     # Verifica se o valor é 3, 6 ou 9 e retorna o meme correspondente
     if valor == 3:
-        return "Apple"
+        return "Meme: https://youtu.be/97JDCEN80yM?si=nTk9HUXRTsgenyXi"
     elif valor == 6:
-        return "Cherry"
+        return "Meme: https://youtu.be/daFzNfSK3b4?si=Vv3J3rFxE2i9VCZA"
     elif valor == 9:
-        return "Lemon"
+        return "Meme: https://youtu.be/dQw4w9WgXcQ?si=im3qezEJO_Mwh7to"
     else:
-        return "errou..."
+        return "Droga..."
 
 # Entrada
-def Leia():         
+def Leia():         # Loop principal do jogo, onde o usuário pode escolher entre girar ou sair
     while True:
-        print("\n--- Gambling ---")
+        print("\n--- Meme Gambling ---")
         print("R - Girar")
         print("SAIR - Encerrar")
         
         opcao = input("Escolha uma opção: ").upper()
         
         if opcao == 'R':
-            A = random.randint(1, 3)        
+            A = random.randint(1, 3)        # Gera números aleatórios entre 1 e 3
             B = random.randint(1, 3)
             C = random.randint(1, 3)
             print(f"Número sorteado: {A}")
             print(f"Número sorteado: {B}")
             print(f"Número sorteado: {C}")
             
-            resultado = Soma(A, B, C)     
+            resultado = Soma(A, B, C)       # Calcula a soma dos números sorteados
             Escreva("Soma", resultado)
             
-            answer = A + B + C         
+            answer = A + B + C          # Calcula a soma dos números sorteados
             if answer == 3 or answer == 6 or answer == 9:
                 print("Parabéns, você acertou o número!")
             else:
                 print("Que pena, tente novamente!")
             
-            fruits = verificar_ganha(answer)
-            print("FRUIT SORTEADA:", fruits)
+            memes = verificar_ganha(answer)
+            print("MEME SORTEADA:", memes)
             
             # Aqui ele envia para a função salvar que agora funciona
             salvar(resultado)
             
-        elif opcao == 'SAIR':       
+        elif opcao == 'SAIR':       # Se o usuário escolher sair, o loop é encerrado e uma mensagem de despedida é exibida
             print("Saindo do jogo. Até a próxima!")
             break
 
