f7a7bb111bd23c41caa129777b44db0f59931881
20251ctb0100035-ai <20251ctb0100035@estudantes.ifpr.edu.br>
2026-09-22 15:16:28 +0000

    Pending changes exported from your codespace

---
+++ aula32-projeto-secretaria/Aluno.java
@@ -0,0 +1,43 @@
+/*
+ * Disciplina   : 2026 - PS
+ * Estudante    : Anthony Pagani
+ * Data         : 13/08/2026
+ * Projeto      : aula32-projeto-secretaria
+ * Arquivo      : Aluno.java
+*/
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private String curso;
+
+    public Aluno(String nome, String matricula, String curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setNome(String curso){
+        this.nome = nome;
+    }
+
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+
+    public String toString() {
+        return matricula + " | " + nome + " | " + curso;
+    }
+}
